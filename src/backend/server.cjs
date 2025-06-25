5fe2e5886c466827efa46a901c85de8ff08208a4
Toufiq <muheeth312@gmail.com>
2025-06-25 17:44:21 +0530

    changes

--- src/backend/server.cjs
+++ src/backend/server.cjs
@@ -9,7 +9,7 @@ let a=async ()=>{
 }
 a()
 app.use(cors({
-    origin:["https://quizs-1.onrender.com/","http://localhost:5173"]
+    origin:["https://quizs-1.onrender.com","http://localhost:5173"]
 }))
 app.use(express.json())
 app.use("/user",user)
