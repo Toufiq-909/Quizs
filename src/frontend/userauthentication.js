04c17a87fea303d8ca92997fb07c8081085af892
Toufiq <muheeth312@gmail.com>
2025-06-25 17:19:01 +0530

    changes

--- src/frontend/userauthentication.js
+++ src/frontend/userauthentication.js
@@ -1,10 +1,11 @@
 import {validpassword,validmail,validusername} from "./validation.js"
+let api_url=import.meta.env.VITE_apicall
 export  async function name(a) {
     let result = validusername.safeParse(a);
 
     if (result.success) {
         console.log(a)
-        let res=await fetch("http://localhost:3000/user/username?name="+a);
+        let res=await fetch(api_url+"/user/username?name="+a);
         if(res.status==200)
         {
             localStorage.setItem("name",a);
@@ -22,7 +23,7 @@ export async function usermail(b)
     let result=validmail.safeParse(b);
     if(result.success)
     {
-        let res=await fetch("http://localhost:3000/user/mail?mail="+b);
+        let res=await fetch(api_url+"/user/mail?mail="+b);
         if(res.status==200)
         {
             localStorage.setItem("mail",b);
@@ -50,7 +51,7 @@ export async function pass(c)
 }
 export async function create(b)
 {
-    let res=await fetch("http://localhost:3000/user/otp",{
+    let res=await fetch(api_url+"/user/otp",{
         method:"Post",
         headers:{
             "Content-Type":"application/json"
